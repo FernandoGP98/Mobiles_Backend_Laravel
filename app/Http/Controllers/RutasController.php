bc8934b53f003290ea2f444ab647c5e4fd9f14dc
Fernando <fer_char98@hotmail.com>
2020-12-02 17:50:47 -0600

    Post get de prueba

--- app/Http/Controllers/RutasController.php
+++ app/Http/Controllers/RutasController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RutasController extends Controller
 {
-    public function getUsuarioPorId(){
-        return '{"resultado":"Correcto", "th":'.$usuario->toJson().'}';
+    public function getPrueba(){
+        $prueba = DB::select('select * from prueba');
+        $response["prueba"]=$prueba;
+        $response["succes"]=1;
+        return response()->json($response);
+        //return '{"resultado":"Correcto", "th":'.$usuario->toJson().'}';
+    }
+
+    public function registrarPrueba(Request $request){
+        $res = DB::insert('insert into prueba (texto) values (?)', [$request->texto]);
+        if($res>0){
+            $response["succes"]=1;
+            return response()->json($response);
+        }else{
+            $response["succes"]=0;
+            return response()->json($response);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,21 @@ use App\Http\Controllers\Usuario;
 |
 */
 
-//Route::get('/login', [Usuario::class, 'index']);
-Route::resource('/login', Usuario::class);
+/*
+PARA VER REFLEJADOS LOS POST USEN https://reqbin.com/
+SI SOLO PONEN LA URL LES SALDRA ESTE ERROR
+"The GET method is not supported for this route. Supported methods: POST."
+NO SE PREOCUPEN SI JALA SOLO NO SE PUEDE ACCEDER CON SOLO PONER LA URL
+POR CIERTO, JALA EN MODO LOCAL
+"php artisan serve"<- En la consola
+
+
+CADA QUE AGREGUEN UNA NUEVA RUTA '/ejemplo' Y SEA POR MEDIO DE POST
+DEBEN AGREGARLA TAMBIEN AL ARCHIVO 'app\Http\Middleware\VerifyCsrfToken.php'
+AHI YA ESTAN LAS DE PRUEBA PARA QUE VEAN COMO SE AGREGAN
+LOS GET NO REQUIEREN ESTE PASO.
+*/
+
+Route::post('/getPrueba', 'RutasController@getPrueba');
+Route::post('/registrarPrueba','RutasController@registrarPrueba');
+//Route::post('/login', 'Usuario');
