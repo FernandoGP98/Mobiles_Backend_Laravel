463becd8a9d041d7160cf50530cd6a580a916c80
Fernando <fer_char98@hotmail.com>
2020-12-03 18:39:01 -0600

    era 0 lol

--- app/Http/Controllers/RutasController.php
+++ app/Http/Controllers/RutasController.php
@@ -57,7 +57,7 @@ class RutasController extends Controller
     }
 
     public  function RestaurantesGetAllPublicados(){
-        $res = Restaurante::select('id','nombre','descripcion', 'calificacion')->where('estado', 1)->get();
+        $res = Restaurante::select('id','nombre','descripcion', 'calificacion')->where('estado', 0)->get();
         if($res->count()>0){
             $response["restaurantes"]=$res;
             $response["success"]=1;
